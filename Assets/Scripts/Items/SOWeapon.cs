decdce17ed322d2df72f9f0546aad57e3bce0a13
Queejon <queejonjin@gmail.com>
2023-04-07 16:34:42 -0400

    feat: item scriptable objects
    Implemented the ScriptableObject classes, and placed stand-in assets to indicate the folder structure of the items for when they are later filled out.

---
+++ Assets/Scripts/Items/SOWeapon.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Weapon", menuName = "Stats/Weapon")]
+public class SOWeapon : ScriptableObject
+{
+    
+}
