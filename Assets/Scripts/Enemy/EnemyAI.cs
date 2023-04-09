681a91ce453f4e8483a5344ccb4d70ebc7735ec6
InfamousForrest <106617744+InfamousForrest@users.noreply.github.com>
2023-04-08 22:31:46 -0400

    feat: fixed variables, added methods
    I made all the variables in EnemyAI fit the naming scheme properly.  Also, I added methods to make IDamageable work.  Finally, I changed the parameters in IDamageable to ints for easier functionality.

--- Assets/Scripts/Enemy/EnemyAI.cs
+++ Assets/Scripts/Enemy/EnemyAI.cs
@@ -2,31 +2,124 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UIElements;
 
 public class EnemyAI : MonoBehaviour, IDamageable
 {
     [Header("--- Components ---")]
-    [SerializeField] Renderer model;
+    [SerializeField] Renderer _model;
+    [SerializeField] NavMeshAgent _agent;
+    [SerializeField] Transform _headPos;
+    [SerializeField] Transform _shootPos;
 
     [Header("--- Enemy Stats ---")]
-    [Range(50, 200)][SerializeField] float _HPMax;
-    private float _HPCurrent;
+    [Range(1, 100)][SerializeField] int _HPMax;
+    [SerializeField] int _facePlayerSpeed;
+    [SerializeField] int _visionAngle;
+    int _HPCurrent;
 
-    // Start is called before the first frame update
+    [Header("--- Gun Stats ---")]
+    [Range(1, 10)][SerializeField] int _shotDamage;
+    [Range(0.1f, 5)][SerializeField] float _fireRate;
+    [Range(1, 100)][SerializeField] int _shootDist;
+    [SerializeField] GameObject _bullet;
+    [SerializeField] int _bulletSpeed;
+
+    Vector3 _playerDir;
+    bool _isPlayerInRange;
+    float _angleToPlayer;
+    bool _isShooting;
+    float _stoppingDistOG;
+
     void Start()
     {
-
+        _HPCurrent = _HPMax;
+        _stoppingDistOG = _agent.stoppingDistance;
     }
 
-    // Update is called once per frame
     void Update()
     {
-
+        if (_isPlayerInRange)
+        {
+            CanSeePlayer();
+        }
     }
 
-    public void Damage(float amount)
+    bool CanSeePlayer()
     {
-        _HPCurrent -= amount;
+        _playerDir = (GameManager.instance.player.transform.position - _headPos.position);
+        _angleToPlayer = Vector3.Angle(new Vector3(_playerDir.x, 0, _playerDir.z), transform.forward);
+
+        Debug.DrawRay(_headPos.position, _playerDir, Color.yellow);
+        Debug.Log(_angleToPlayer);
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(_headPos.position, _playerDir, out hit))
+        {
+            if (hit.collider.CompareTag("Player") && _angleToPlayer <= _visionAngle)
+            {
+                _agent.stoppingDistance = _stoppingDistOG;
+                _agent.SetDestination(GameManager.instance.player.transform.position);
+
+                if (_agent.remainingDistance < _agent.stoppingDistance)
+                    FacePlayer();
+
+                if (!_isShooting)
+                    StartCoroutine(FireShot());
+
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _isPlayerInRange = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _isPlayerInRange = false;
+        }
+    }
+
+    IEnumerator FireShot()
+    {
+        _isShooting = true;
+
+        GameObject bulletClone = Instantiate(_bullet, _shootPos.position, _bullet.transform.rotation);
+        bulletClone.GetComponent<Rigidbody>().velocity = transform.forward * _bulletSpeed;
+
+        yield return new WaitForSeconds(_fireRate);
+        _isShooting = false;
+    }
+
+    IEnumerator flashColor(Color clr)
+    {
+        Color mainColor = _model.material.color;
+        _model.material.color = clr;
+        yield return new WaitForSeconds(0.1f);
+        _model.material.color = mainColor;
+    }
+
+    void FacePlayer()
+    {
+        Quaternion rot = Quaternion.LookRotation(new Vector3(_playerDir.x, 0, _playerDir.z));
+        transform.rotation = Quaternion.Lerp(transform.rotation, rot, Time.deltaTime * _facePlayerSpeed);
+    }
+
+    public void Damage(int amount)
+    {
+        _HPCurrent -= amount;        
         StartCoroutine(flashColor(Color.red));
 
         if (_HPCurrent <= 0)
@@ -35,15 +128,7 @@ public class EnemyAI : MonoBehaviour, IDamageable
         }
     }
 
-    IEnumerator flashColor(Color clr)
-    {
-        Color mainColor = model.material.color;
-        model.material.color = clr;
-        yield return new WaitForSeconds(0.1f);
-        model.material.color = mainColor;
-    }
-
-    public void Heal(float health)
+    public void Heal(int health)
     {
         _HPCurrent += health;
         StartCoroutine(flashColor(Color.green));
