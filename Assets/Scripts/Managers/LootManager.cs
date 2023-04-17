62b9b6121efbbe66e47827d56d514ae042bea30f
Queejon <queejonjin@gmail.com>
2023-04-16 22:49:12 -0400

    feat: FX asset imported, assets upgraded to URP, and loot location generation

---
+++ Assets/Scripts/Managers/LootManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LootManager : MonoBehaviour
+{
+    [SerializeField]
+    private int _lootCount;
+    [SerializeField]
+    private Bounds _spawningBounds;
+    private List<Transform> _lootLocations;
+    
+    private void Start()
+    {
+        SetUpStage();
+    }
+
+#if UNITY_EDITOR
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.grey;
+        Gizmos.DrawWireCube(_spawningBounds.center, _spawningBounds.size);
+        if (!Application.isPlaying) return;
+
+        Gizmos.color = Color.black;
+        foreach(Transform trans in _lootLocations)
+        {
+            Gizmos.DrawSphere(trans.position, 1);
+        }
+    }
+#endif
+
+    private void SetUpStage()
+    {
+        GenerateLootLocations();
+    }
+
+    private void GenerateLootLocations()
+    {
+        _lootLocations = new(_lootCount);
+        Vector2 location = new();
+        for(int i = 0; i < _lootCount; ++i)
+        {
+            location.x = Random.Range(_spawningBounds.min.x, _spawningBounds.max.x);
+            location.y = Random.Range(_spawningBounds.min.z, _spawningBounds.max.z);
+            _lootLocations.Add(new GameObject().transform);
+            _lootLocations[^1].name = $"Loot item ({i + 1})";
+            _lootLocations[^1].position = new Vector3(location.x, 1, location.y);
+        }
+    }
+}
