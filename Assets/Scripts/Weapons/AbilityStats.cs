a6ad78479b9a9470d1f415f536b4e6cb12ec7f97
Queejon <queejonjin@gmail.com>
2023-04-23 23:44:31 -0400

    feat: FIREBALL

--- Assets/Scripts/Weapons/AbilityStats.cs
+++ Assets/Scripts/Weapons/AbilityStats.cs
@@ -5,9 +5,12 @@ public struct AbilityStats
 {
     public GameObject prefab;
     public float damage;
+    public float directDamage;
     public float cooldown;
     public float lifetime;
+    public float secondaryLifetime;
     public float speed;
+    public float fxIntesity;
 
 
     public static AbilityStats operator +(AbilityStats s1, AbilityStats s2)
@@ -16,9 +19,12 @@ public struct AbilityStats
 
         stats.prefab = s1.prefab;
         stats.damage = s1.damage + s2.damage;
+        stats.directDamage = s1.directDamage + s2.directDamage;
         stats.cooldown = s1.cooldown + s2.cooldown;
         stats.lifetime = s1.lifetime + s2.lifetime;
+        stats.secondaryLifetime = s1.secondaryLifetime + s2.secondaryLifetime;
         stats.speed = s1.speed + s2.speed;
+        stats.fxIntesity = s1.fxIntesity+ s2.fxIntesity;
 
         return stats;
     }
--- Assets/Scripts/Weapons/FireProjectile.cs
+++ Assets/Scripts/Weapons/FireProjectile.cs
@@ -4,5 +4,58 @@ using UnityEngine;
 
 public class FireProjectile : Projectile
 {
+    [SerializeField]
+    private bool _isAOE;
+    private bool _exploding;
+    private List<IDamageable> _hitEnemies = new();
 
+    protected override void Start()
+    {
+        StartCoroutine(DoDestroy());
+        GetComponent<Rigidbody>().velocity = transform.forward * _stats.speed;
+    }
+
+    protected override void OnTriggerEnter(Collider other)
+    {
+        if(!_exploding)
+        {
+            if (other.gameObject.TryGetComponent<IDamageable>(out var damageable))
+            {
+                damageable.Damage(_stats.directDamage);
+            }
+            StartCoroutine(DoExplode());
+        }
+        else if(other.gameObject.TryGetComponent<IDamageable>(out var damageable) && !_hitEnemies.Contains(damageable))
+        {
+            _hitEnemies.Add(damageable);
+            damageable.Damage(_stats.damage);
+        }
+
+        if (!_isAOE)
+            Destroy(gameObject);
+    }
+
+    protected virtual IEnumerator DoDestroy()
+    {
+        yield return new WaitForSeconds(_stats.lifetime);
+        if (!_exploding)
+            Destroy(gameObject);
+    }
+
+    protected virtual IEnumerator DoExplode()
+    {
+        _exploding = true;
+        GetComponent<Rigidbody>().velocity = Vector3.zero;
+        Destroy(gameObject, _stats.secondaryLifetime);
+        while(true)
+        {
+            transform.localScale += Vector3.one * _stats.fxIntesity;
+            yield return new WaitForSeconds(Time.deltaTime);
+        }
+    }
+
+    protected virtual void OnDestroy()
+    {
+        StopAllCoroutines();
+    }
 }
