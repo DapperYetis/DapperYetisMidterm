4158a9f8949d148d41d2822587cb06a773549e29
PJays69 <jsilvaisyourbro@gmail.com>
2023-04-26 19:27:10 -0400

    UI Polished
    Item HUD size has been reduced to allow more visibility for the player.

--- Assets/Scripts/Weapons/GrappleCamera.cs
+++ Assets/Scripts/Weapons/GrappleCamera.cs
@@ -13,11 +13,6 @@ public class GrappleCamera : MonoBehaviour
     float yRotation;
 
     // Start is called before the first frame update
-    void Start()
-    {
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
-    }
 
     // Update is called once per frame
     void Update()
