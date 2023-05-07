a00ea6148c922263a89a22953b923ef9c5657cf7
Queejon <queejonjin@gmail.com>
2023-05-07 02:00:19 -0400

    feat: item effect structure and example 1

---
+++ Assets/Scripts/Loadout/DirtProjectile.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class DirtProjectile : Projectile
+{
+    protected Rigidbody _rb;
+    protected EnemyAI _target;
+
+    protected override void Start()
+    {
+        base.Start();
+        _rb = GetComponent<Rigidbody>();
+        _target = (from enemy in EnemyManager.instance.enemies orderby (enemy.transform.position - transform.position).magnitude / Vector3.Dot(enemy.transform.position - transform.position.normalized, transform.forward) ascending select enemy).First();
+    }
+
+    private void FixedUpdate()
+    {
+        if(_target != null)
+            _rb.velocity += (_target.transform.position - transform.position).normalized * (_stats.speed * Time.fixedDeltaTime);
+    }
+}
