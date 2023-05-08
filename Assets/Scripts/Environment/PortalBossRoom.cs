47d79ce35ec19ae0464ff38f810da8401e5224e8
InfamousForrest <106617744+InfamousForrest@users.noreply.github.com>
2023-05-08 15:37:57 -0400

    Area change portals (#15)
    * feat: Added all the new boss base prefabs with their associated waves, the base portals for changing scenes and their scripts
    
    * feat: most of the area transfer logic is implemented.  Needs testing to make sure it works
    
    * feat: significant updates to the scene changing scripts and the portals.  Added the Boss Room Scene to the Build Settings.  Renamed the prefabs and scripts to be more readable.
    
    * feat: Finished! Portals, scene changes, and prefabs are all set up properly.

---
+++ Assets/Scripts/Environment/PortalBossRoom.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalBossRoom : MonoBehaviour
+{
+    [Header("--- Components ---")]
+    [SerializeField]
+    protected GameObject _portalEnterObject;
+    [SerializeField]
+    protected GameObject _portalExitObject;
+    [SerializeField]
+    protected SphereCollider _proclamationCollider;
+    [SerializeField]
+    protected AudioSource _aud;
+    [SerializeField]
+    protected SOWave _bossWave;
+    [SerializeField]
+    protected Transform _bossSpawnPoint;
+
+    bool _bossHasSpawned;
+
+    [Header("--- Audio Controls ---")]
+    [Range(0, 1)]
+    [SerializeField]
+    protected float _audPortalVol;
+    [SerializeField]
+    protected AudioClip[] _audPortal;
+    [Range(0, 1)]
+    [SerializeField]
+    protected float _audBossExclamationVol;
+    [SerializeField]
+    protected AudioClip[] _audBossExclamation;
+
+    private void Start()
+    {
+        StartCoroutine(PlayerEntry());
+    }
+
+    private void Update()
+    {
+        if (_bossHasSpawned && EnemyManager.instance.enemies.Count < 1 && !_portalExitObject.activeInHierarchy)
+        {
+            PlayerWon();
+        }
+    }
+
+    private IEnumerator PlayerEntry()
+    {
+        _aud.PlayOneShot(_audPortal[Random.Range(0, _audPortal.Length)], _audPortalVol);
+        Debug.Log($"{name} played a sound");
+        if (!EnemyManager.instance.inBossRoom)
+            EnemyManager.instance.EnterBossRoom(null);
+        yield return new WaitForSeconds(3);
+        _portalEnterObject.SetActive(false);
+    }
+
+    private void PlayerWon()
+    {
+        _aud.PlayOneShot(_audPortal[Random.Range(0, _audPortal.Length)], _audPortalVol);
+        Debug.Log($"{name} played a sound");
+        _portalExitObject.SetActive(true);
+        RenderSettings.fog = false;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log(other.gameObject);
+        if (other.gameObject.layer == 6)
+        {
+            _aud.PlayOneShot(_audBossExclamation[Random.Range(0, _audBossExclamation.Length)], _audBossExclamationVol);
+            Debug.Log($"{name} played a sound");
+            _proclamationCollider.enabled = false;
+
+            EnemyManager.SpawnEnemy(_bossWave, _bossSpawnPoint.position);
+            _bossHasSpawned = true;
+        }
+    }
+}
