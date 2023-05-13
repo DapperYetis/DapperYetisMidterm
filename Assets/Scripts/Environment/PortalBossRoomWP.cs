a569510901ece5580748429887a0b84f297b0892
InfamousForrest <106617744+InfamousForrest@users.noreply.github.com>
2023-05-13 03:39:22 -0400

    feat: Boss Room portal is fully particled out and operational

---
+++ Assets/Scripts/Environment/PortalBossRoomWP.cs
@@ -0,0 +1,112 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalBossRoomWP : MonoBehaviour
+{
+    private static int _bossIndex = 0;
+    public static int bossIndex => _bossIndex;
+    private static int _totalBosses;
+    public static int totalBosses => _totalBosses;
+
+    [Header("--- Components ---")]
+    [SerializeField]
+    protected GameObject _portalEnterIdle;
+    [SerializeField]
+    protected GameObject _portalEnterClose;
+    [SerializeField]
+    protected GameObject _portalExitOpen;
+    [SerializeField]
+    protected GameObject _portalExitIdle;
+    [SerializeField]
+    protected SphereCollider _proclamationCollider;
+    [SerializeField]
+    protected AudioSource _aud;
+    [SerializeField]
+    protected SOWave[] _bossWaves;
+    [SerializeField]
+    protected Transform _bossSpawnPoint;
+
+    bool _bossHasSpawned;
+
+    [Header("--- Audio Controls ---")]
+    [Range(0, 1)]
+    [SerializeField]
+    protected float _audPortalVol;
+    [SerializeField]
+    protected AudioClip[] _audPortal;
+    [Range(0, 1)]
+    [SerializeField]
+    protected float _audBossExclamationVol;
+    [SerializeField]
+    protected AudioClip[] _audBossExclamation;
+
+    float _entryIdleDur;
+    float _entryCloseDur;
+    float _exitIdleDur;
+    float _exitOpenDur;
+    Portal _whereThePortalGoing;
+
+    private void Start()
+    {
+        _whereThePortalGoing = _portalExitIdle.GetComponent<Portal>();
+        _entryIdleDur = _portalEnterIdle.GetComponent<ParticleSystem>().main.duration;
+        _entryCloseDur = _portalEnterClose.GetComponent<ParticleSystem>().main.duration;
+        _exitOpenDur = _portalExitOpen.GetComponent<ParticleSystem>().main.duration;
+        _exitIdleDur = _portalExitIdle.GetComponent<ParticleSystem>().main.duration;
+        _totalBosses = _bossWaves.Length;
+        StartCoroutine(PlayerEntry());
+    }
+
+    private void Update()
+    {
+        if (_bossHasSpawned && EnemyManager.instance.enemies.Count < 1 && !_portalExitOpen.activeInHierarchy && !_portalExitIdle.activeInHierarchy)
+        {
+            PlayerWon();
+        }
+    }
+
+    private IEnumerator PlayerEntry()
+    {
+        _aud.PlayOneShot(_audPortal[Random.Range(0, _audPortal.Length)], _audPortalVol);
+        //Debug.Log($"{name} played a sound");
+        if (!EnemyManager.instance.inBossRoom)
+            EnemyManager.instance.EnterBossRoom(null);
+        yield return new WaitForSeconds(_entryIdleDur);
+        _portalEnterIdle.SetActive(false);
+        _portalEnterClose.SetActive(true);
+        yield return new WaitForSeconds(_entryCloseDur);
+        _portalEnterClose.SetActive(false);
+    }
+
+    private void PlayerWon()
+    {
+        _aud.PlayOneShot(_audPortal[Random.Range(0, _audPortal.Length)], _audPortalVol);
+        //Debug.Log($"{name} played a sound");
+        StartCoroutine(PortalOpening());
+        RenderSettings.fog = false;
+    }
+
+    private IEnumerator PortalOpening()
+    {
+        _portalExitOpen.SetActive(true);
+        yield return new WaitForSeconds(_exitOpenDur);
+        _portalExitOpen.SetActive(false);
+        _portalExitIdle.SetActive(true);
+        _whereThePortalGoing.SetBuildIndex(++_bossIndex + 1);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log(other.gameObject);
+        if (other.gameObject.layer == 6)
+        {
+            _aud.PlayOneShot(_audBossExclamation[Random.Range(0, _audBossExclamation.Length)], _audBossExclamationVol);
+            Debug.Log($"{name} played a sound");
+            _proclamationCollider.enabled = false;
+
+            EnemyManager.SpawnEnemy(_bossWaves[_bossIndex], _bossSpawnPoint.position);
+            _bossHasSpawned = true;
+        }
+    }
+}
