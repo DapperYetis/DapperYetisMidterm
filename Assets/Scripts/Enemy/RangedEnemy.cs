49e5c78e39db39f957f6e3f2d8373528e3af46ad
InfamousForrest <106617744+InfamousForrest@users.noreply.github.com>
2023-05-16 17:43:53 -0400

    Implementing all enemies (#41)
    * feat: completed Spike, removed all animations that have a Root
    
    * feat: completed the Snakes (they are awesome!!!), removed the extra portal prefabs and scripts, updated the speeds of all the enemies
    
    * feat: re-put the portal's materials on.
    
    No idea how they got removed.
    
    * feat: balanced snake wave, made a script for the Snake Naga specifically
    
    * feat: Blossom has completely implemented stats, animations, and sound banks.
    
    I discovered that I may have deleted most of the werewolf stuff when I cleared the unwanted enemies.  I also now know where the problem "Blooms not moving" problem should be.
    
    * feat: Blooms and Blossoms, otherwise known as pure ranged enemies can now move.
    
    Their walk animations are not well synced with their movement speed.  Their movement is currently only of the hard-locked run at the player sort.
    
    * feat: Poison Bomb is partially implemented.  Ranged Enemies only move toward the player.  Naga Boss starts here.
    
    I didn't have time to make Poison Bomb's movement or explosion.
    I couldn't make Ranged enemies's movement be varied, it didn't work.
    
    * feat: The Naga Boss was underway until I was informed that it wouldn't be necessary.  So i'm not completing it for now.

--- Assets/Scripts/Enemy/RangedEnemy.cs
+++ Assets/Scripts/Enemy/RangedEnemy.cs
@@ -7,12 +7,42 @@ public class RangedEnemy : EnemyAI
 {
     protected override void Update()
     {
-        base.Update();
-
         if (!_isAttacking)
         {
             _isAttacking = true;
             EnemyManager.instance.QueueAttack(Shoot, AttackPriority, this);
+        }
+
+        base.Update();
+    }
+
+    protected override void Movement()
+    {
+        FacePlayer();
+
+        //if ((GameManager.instance.player.transform.position - transform.position).magnitude <= _agent.stoppingDistance)
+        //{
+        //    Vector3 normDir = (GameManager.instance.player.transform.position - transform.position).normalized;
+        //
+        //    _agent.SetDestination(transform.position - (normDir * _speed));
+        //}
+        //else
+        //{
+            _agent.SetDestination(GameManager.instance.player.transform.position);
+        //}
+    }
+
+    protected override float AttackPriority()
+    {
+        return base.AttackPriority() + _primaryAttackStats.range - GameManager.instance.player.transform.position.magnitude;
+    }
+
+    public override void ScaleEnemy()
+    {
+        base.ScaleEnemy();
+        for (int i = 0; i < EnemyManager.instance.scaleFactorInt - 1; ++i)
+        {
+            _primaryAttackStats += _primaryAttackStatsScaling;
         }
     }
 
@@ -26,14 +56,14 @@ public class RangedEnemy : EnemyAI
     protected virtual IEnumerator FireShot()
     {
         _anim.SetTrigger("Shoot");
-        if(_primaryAttackStats._attackAudio.Length > 0)
+        if (_primaryAttackStats._attackAudio.Length > 0)
         {
             if (_primaryAttackStats._attackAudio.Length > 0)
                 _aud.PlayOneShot(_primaryAttackStats._attackAudio[Random.Range(0, _primaryAttackStats._attackAudio.Length)], _primaryAttackStats._attackAudioVol);
             else
                 Debug.LogWarning("No Ranged Attack Sounds to play!");
         }
-        
+
         Quaternion rot = Quaternion.LookRotation(_playerDirProjected * 0.5f);
         if (Mathf.Abs(Quaternion.Angle(rot, Quaternion.LookRotation(_playerDir))) >= 60)
             rot = Quaternion.LookRotation(_playerDir);
