a0cf920ae9ccad8fb42b19ed937dadaded3b9a99
HannaM06 <116569149+HannaM06@users.noreply.github.com>
2023-05-17 00:51:48 -0400

    Player vfx (#48)
    * feat: created VFX for shield secondary and teleport primary and secondary.
    
    * feat: fixed grapple to point
    
    * weapon effect, etc.
    
    * feat: fixed fire staff. primary and secondary work again
    
    * merged master part 2
    
    * feat: audio set for player and two staffs
    
    * feat: Audio for two staff and three supports

--- Assets/Scripts/Loadout/FireStaff.cs
+++ Assets/Scripts/Loadout/FireStaff.cs
@@ -1,27 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static Unity.VisualScripting.Member;
 
-public class FireStaff : Staff
+public class FireStaff : Weapon
 {
     [SerializeField]
     protected ProjectileMelee _flame;
 
+    [Header("-----Audio-----")]
+    [SerializeField] AudioSource _audio;
+    [SerializeField] AudioClip[] _primFireWeaponAud;
+    [SerializeField][Range(0f, 1f)] float _primFireWeaponAudVol;
+    [SerializeField] AudioClip[] _secFireWeaponAud;
+    [SerializeField][Range(0f, 1f)] float _secFireWeaponAudVol;
 
     protected override void Update()
     {
         if (Input.GetButtonDown("Primary Fire") && _canUsePrimary)
         {
             _flame.gameObject.SetActive(true);
+            _audio.loop = false;
+            if (_primFireWeaponAud.Length > 0)
+            {
+                _audio.clip = _primFireWeaponAud[Random.Range(0, _primFireWeaponAud.Length)];
+                _audio.loop = true;
+                _audio.Play();
+            }
             _flame.SetStats(_stats.primaryAbility);
             _canUsePrimary = false;
         }
         else if (Input.GetButton("Secondary Fire") && _canUseSecondary && !GameManager.instance.isPaused)
+        {
+            if (_secFireWeaponAud.Length > 0)
+                _audio.PlayOneShot(_secFireWeaponAud[Random.Range(0, _secFireWeaponAud.Length)], _secFireWeaponAudVol);
             StartCoroutine(SecondaryFire());
+        }
         if (Input.GetButtonUp("Primary Fire") && !_canUsePrimary)
         {
             _flame.gameObject.SetActive(false);
+            _audio.loop = false;
             _canUsePrimary = true;
         }
+        base.Update();
     }
 }
--- Assets/Scripts/Loadout/Staff.cs
+++ Assets/Scripts/Loadout/Staff.cs
@@ -4,5 +4,25 @@ using UnityEngine;
 
 public class Staff : Weapon
 {
-    
+    [Header("-----Audio-----")]
+    [SerializeField] AudioSource _audio;
+    [SerializeField] AudioClip[] _primaryIceAud;
+    [SerializeField][Range(0f, 1f)] float _primIceWeaponAudVol;
+    [SerializeField] AudioClip[] _secIceWeaponAud;
+    [SerializeField][Range(0f, 1f)] float _secIceWeaponAudVol;
+
+    protected override void Update()
+    {
+        if (Input.GetButton("Primary Fire") && _canUsePrimary && !GameManager.instance.isPaused)
+        {
+            if (_primaryIceAud.Length > 0)
+                _audio.PlayOneShot(_primaryIceAud[Random.Range(0, _primaryIceAud.Length)], _primIceWeaponAudVol);
+        }
+        else if (Input.GetButton("Secondary Fire") && _canUseSecondary && !GameManager.instance.isPaused)
+        {
+            if (_secIceWeaponAud.Length > 0)
+                _audio.PlayOneShot(_secIceWeaponAud[Random.Range(0, _secIceWeaponAud.Length)], _secIceWeaponAudVol);
+        }
+        base.Update();
+    }
 }
