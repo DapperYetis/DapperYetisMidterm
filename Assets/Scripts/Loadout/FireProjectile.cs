bdec126e078f2ed3e75a8672ad2a4679ed6e0bb2
Queejon <queejonjin@gmail.com>
2023-05-17 02:26:25 -0400

    feat: final build fixes

--- Assets/Scripts/Loadout/FireProjectile.cs
+++ Assets/Scripts/Loadout/FireProjectile.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class FireProjectile : Projectile
@@ -11,7 +12,15 @@ public class FireProjectile : Projectile
     {
         if (other.isTrigger) return;
 
-        Instantiate(_explosionPrefab, transform.position, Quaternion.identity).GetComponent<Projectile>().SetStats(_stats);
+        if(_explosionPrefab != null)
+            Instantiate(_explosionPrefab, transform.position, Quaternion.identity).GetComponent<Projectile>().SetStats(_stats);
+        else
+        {
+            if(other.TryGetComponent<IBuffable>(out var buffable))
+            {
+                buffable.Damage(_stats.directDamage, (from buff in _stats.targetBuffs select (buff, 1)).ToArray());
+            }
+        }
 
         Destroy(gameObject);
     }
