117c93e538ccbc508f195071d0d5bbe1f3b38765
InfamousForrest <106617744+InfamousForrest@users.noreply.github.com>
2023-05-16 20:31:10 -0400

    feat: Poison Bomb has a script for the fuse burning audio on it's actual fuse.  Also added light flick sounds for the getting close sound.

--- Assets/Scripts/Enemy/All Individual Enemies/PoisonBomb.cs
+++ Assets/Scripts/Enemy/All Individual Enemies/PoisonBomb.cs
@@ -4,5 +4,16 @@ using UnityEngine;
 
 public class PoisonBomb : MeleeEnemy
 {
-    
+    protected override void Movement()
+    {
+        FacePlayer();
+        _agent.SetDestination(GameManager.instance.player.transform.position);
+
+        if ((GameManager.instance.player.transform.position - transform.position).magnitude < 10)
+        {
+            _aud.PlayOneShot(_audGettingClose[Random.Range(0, _audGettingClose.Length)], _audGettingCloseVol);
+        }
+    }
+
+
 }
---
+++ Assets/Scripts/Enemy/FuseBurn.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FuseBurn : MonoBehaviour
+{
+    AudioSource _audFuse;
+    
+    void Start()
+    {
+        _audFuse = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_audFuse.isActiveAndEnabled)
+        {
+            if (!_audFuse.isPlaying && (GameManager.instance.player.transform.position - transform.position).magnitude <= 10)
+            {
+                _audFuse.Play();
+            }
+            else if (_audFuse.isPlaying && (GameManager.instance.player.transform.position - transform.position).magnitude > 10)
+            {
+                _audFuse.Stop();
+            }
+        }
+    }
+}
