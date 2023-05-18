53dc1f2feaed5d922c4d67bf9f3eb7ea6f0330db
HannaM06 <hannaleemorris@gmail.com>
2023-05-18 17:02:54 -0400

    feat: fixed flame thrower so now the audio stops as soon as the player stops shooting and fixed the visual look of it.

--- Assets/Scripts/Loadout/FireStaff.cs
+++ Assets/Scripts/Loadout/FireStaff.cs
@@ -7,6 +7,8 @@ public class FireStaff : Weapon
 {
     [SerializeField]
     protected ProjectileMelee _flame;
+    private ParticleSystem _particleSystem;
+    public Collider _collider;
 
     [Header("-----Audio-----")]
     [SerializeField] AudioSource _audio;
@@ -14,12 +16,22 @@ public class FireStaff : Weapon
     [SerializeField][Range(0f, 1f)] float _primFireWeaponAudVol;
     [SerializeField] AudioClip[] _secFireWeaponAud;
     [SerializeField][Range(0f, 1f)] float _secFireWeaponAudVol;
+    private void Start()
+    {
+        _collider = _flame.GetComponent<Collider>();
+        _particleSystem = _flame.GetComponent<ParticleSystem>();
+        _collider.enabled = false;
+        _particleSystem.Stop();
+        _audio.Stop();
+    }
 
     protected override void Update()
     {
         if (Input.GetButtonDown("Primary Fire") && _canUsePrimary)
         {
-            _flame.gameObject.SetActive(true);
+            //_flame.gameObject.SetActive(true);
+            _collider.enabled = true;
+            _particleSystem.Play();
             _audio.loop = false;
             if (_primFireWeaponAud.Length > 0)
             {
@@ -38,8 +50,11 @@ public class FireStaff : Weapon
         }
         if (Input.GetButtonUp("Primary Fire") && !_canUsePrimary)
         {
-            _flame.gameObject.SetActive(false);
+            //_flame.gameObject.SetActive(false);
+            _collider.enabled = false;
+            _particleSystem.Stop();
             _audio.loop = false;
+            _audio.Stop();
             _canUsePrimary = true;
         }
         base.Update();
