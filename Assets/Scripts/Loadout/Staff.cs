5d671da4809ac04c16be584a8372f8d727edfda5
HannaM06 <hannaleemorris@gmail.com>
2023-05-26 13:12:20 -0400

    feat: increase flame thrower range

--- Assets/Scripts/Loadout/Staff.cs
+++ Assets/Scripts/Loadout/Staff.cs
@@ -11,17 +11,30 @@ public class Staff : Weapon
     [SerializeField] AudioClip[] _secIceWeaponAud;
     [SerializeField][Range(0f, 1f)] float _secIceWeaponAudVol;
 
+    private void Start()
+    {
+        _audio.volume = 1f;
+    }
+
     protected override void Update()
     {
         if (Input.GetButton("Primary Fire") && _canUsePrimary && !GameManager.instance.isPaused)
         {
             if (_primaryIceAud.Length > 0)
-                _audio.PlayOneShot(_primaryIceAud[Random.Range(0, _primaryIceAud.Length)], _primIceWeaponAudVol);
+            {
+                //_audio.PlayOneShot(_primaryIceAud[Random.Range(0, _primaryIceAud.Length)], _primIceWeaponAudVol);
+                _audio.clip = _primaryIceAud[Random.Range(0, _primaryIceAud.Length)];
+                _audio.Play();
+            }
         }
         else if (Input.GetButton("Secondary Fire") && _canUseSecondary && !GameManager.instance.isPaused)
         {
             if (_secIceWeaponAud.Length > 0)
-                _audio.PlayOneShot(_secIceWeaponAud[Random.Range(0, _secIceWeaponAud.Length)], _secIceWeaponAudVol);
+            {
+                //_audio.PlayOneShot(_secIceWeaponAud[Random.Range(0, _secIceWeaponAud.Length)], _secIceWeaponAudVol);
+                _audio.clip = _secIceWeaponAud[Random.Range(0, _secIceWeaponAud.Length)];
+                _audio.Play();
+            }
         }
         base.Update();
     }
